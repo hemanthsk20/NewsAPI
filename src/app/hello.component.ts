5a96798a63419199b6068a2dad75bcc9390206eb
hemanthsk20 <hemanthskmurthy@gmail.com>
2020-04-15 17:09:15 +0530

    4 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1>Trending {{name}}!</h1>`,
-  styles: [`h1 { font-family: "Bree Serif", serif; background-color: #fff;padding: 2px 0px 2px 20px;border:3px solid #888888; color: #ff7433 }`]
+  styles: [`h1 { font-family: "Bree Serif", serif; background: linear-gradient(279deg,#00a562,#36c275 50%,#00d301); padding: 2px 0px 2px 20px;border:3px solid #fff; color: #fff }`]
 })
 export class HelloComponent  {
   @Input() name: string;
